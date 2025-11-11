11c9db86c273e7be5ef0316a2921f079df887995
Clenilton-Filho <100090985+Clenilton-Filho@users.noreply.github.com>
2025-11-11 18:35:20 -0300

    style: modificando header e mais simplificações

--- task_list/forms.py
+++ task_list/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import re
 
 # Formulário de registro estendendo UserCreationForm para uso simples
 # usando o formulário pronto do django,
@@ -14,3 +16,19 @@ class UserRegistrationForm(UserCreationForm):
 
         # Salva o usuário
         return super().save(commit=commit)
+
+    def clean_password1(self):
+         #Exige pelo menos uma letra maiúscula e um número na senha.
+         password = self.cleaned_data.get('password1')
+         if not password:
+             return password
+
+         # Verifica pelo menos uma letra maiúscula
+         if not re.search(r'[A-Z]', password):
+             raise ValidationError('A senha deve conter ao menos uma letra maiúscula.')
+
+         # Verifica ao menos um dígito
+         if not re.search(r'\\d', password):
+             raise ValidationError('A senha deve conter ao menos um número.')
+
+         return password
