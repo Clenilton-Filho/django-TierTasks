386bec34596f3ddbc722d0a691d8e08b6c492d0b
Clenilton-Filho <100090985+Clenilton-Filho@users.noreply.github.com>
2025-11-12 18:52:45 -0300

    style: página principal responsiva nas principais larguras de tela

---
+++ static/js/config-inline-tailwind.js
@@ -0,0 +1,11 @@
+
+//Configurações personalizadas para o Tailwind
+tailwind.config = {
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1850px' // breakpoint para ~1920px
+      },
+    },
+  },
+};
